af426f20754f4fd1d31c554615652ad4573e2bf4
s-nt-s <santos82h@gmail.com>
2023-08-14 23:58:18 +0200

    sacar la media

--- docs/rec/js/99-main.js
+++ docs/rec/js/99-main.js
@@ -1,7 +1,7 @@
 DB = null;
 const $ = {
     "i": (id) => document.getElementById(id),
-    "s": (qr) => document.querySelectorAll(qr),
+    "s": (qr) => Array.from(document.querySelectorAll(qr)),
     "c": (id) => {
         let el = document.getElementById(id);
         if (el==null) return null;
@@ -11,15 +11,30 @@ const $ = {
     }
 };
 
+const uniq = (arr) => Array.from(new Set(arr)).sort();
+
+function monthDiff(d1, d2) {
+    if (typeof d1 === "string") d1 = new Date(d1);
+    if (typeof d2 === "string") d2 = new Date(d2);
+    let month = (d2.getFullYear() - d1.getFullYear())*12;
+    month -= d1.getMonth();
+    month += d2.getMonth();
+    let days = d2.getDate() - d1.getDate();
+    if (days>15) month++;
+    return month;
+}
+
 function doLoading(b) {
     if (b !== false) b = true;
     document.body.style.display=(b?'none':'');
 }
 
 function sDB(select) {
+    console.log(select);
     const r = DB.exec(select);
     let vals =  r[0].values;
     if (vals.length>0 && vals[0].length==1) vals = vals.map(x=>x[0]);
+    console.log(vals);
     return vals;
 }
 
@@ -49,6 +64,7 @@ function init() {
     const html = [];
     sDB(`
         select id, txt from categoria
+        where id!=-2
         order by txt
     `).forEach(([id, txt], index) => {
         const countcat = sDB(`
@@ -73,7 +89,7 @@ function init() {
             html.push(`
                 <li>
                     <code>${countcat}</code>
-                    <input type="checkbox" value="${ids.join(" ")}" id="cat_${index}"/>
+                    <input checked type="checkbox" value="${ids.join(" ")}" id="cat_${index}"/>
                     <label for="cat_${index}">${txt}</label> 
             `);
             html.push(`<ul>`);
@@ -82,7 +98,7 @@ function init() {
             html.push(`
             <li>
                 <code>${count}</code>
-                <input type="checkbox" value="${sid}" id="sub_${sid}"/>
+                <input checked type="checkbox" value="${sid}" id="sub_${sid}"/>
                 <label for="sub_${sid}">${stxt}</label> 
             </li>
             `);
@@ -96,24 +112,55 @@ function init() {
     $cat.innerHTML = html.join("\n");
     $.s("#cat input").forEach(n => {
         if (n.parentNode.getElementsByTagName("li").length==0) return;
-        n.parentNode.addEventListener("change", (e=> {
-            e.target.parentNode.querySelectorAll("ul li input").forEach(x => {
+        const childs = n.value.split(/\s+/).flatMap(id => $.s(`#cat input[value='${id}']`));
+        const listener = () => {
+            childs.forEach(x => {
                 x.disabled = n.checked;
+                x.checked = n.checked;
             })
-        }));
+        }
+        n.addEventListener("change", listener);
+        listener();
     });
 
     $.s("#ini, #fin, #cat input").forEach(n => {
         n.addEventListener("change", doChange);
     });
+    doChange();
 }
 
 function doChange() {
     const ini = $.i("ini").value;
     const fin = $.i("fin").value;
-    const ids = [... new Set(Array.from($.s("#cat input:checked")).flatMap(n => {
+    const ids = uniq($.s("#cat input:checked").flatMap(n => {
         const val = n.value.split(/\s+/);
         return val.map(v=>Number(v));
-    }))].sort();
-    console.log(ids);
+    }));
+    if (ids.length==0) return;
+    const [gastos, ingreosos] = sDB(`
+        select
+            -sum(case
+                when importe<0 then importe
+                else 0
+            end) gastos,
+            sum(case
+                when importe>0 then importe
+                else 0
+            end) ingresos
+        from
+            movimiento
+        where
+            subcategoria in ('NaN', ${ids.join(", ")}) and
+            fecha>='${ini}' and 
+            fecha<='${fin}'
+    `)[0];
+
+    const m = monthDiff(ini, fin);
+    const $res = $.s("#res > ul")[0];
+    $res.innerHTML=`
+        <li><strong>Meses</strong>: ${m}</li>
+        <li><strong>Ingresos</strong>: ${Math.round(ingreosos/m)} €/mes</li>
+        <li><strong>Gastos</strong>: ${Math.round(gastos/m)} €/mes</li>
+        <li><strong>Ahorro</strong>: ${Math.round((ingreosos-gastos)/m)} €/mes</li>
+    `;
 }
